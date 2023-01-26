90318ca4bf1f4a92f941cbc864b8f0f6515f9663
Vieira Joaquim <vjoaquim1@gmail.com>
2023-01-26 11:33:19 -0300

    Projeto criado

---
+++ src/main/java/com/myprojects/PostsManager/PostsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.myprojects.PostsManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myprojects/PostsManager/PostsManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.PostsManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
