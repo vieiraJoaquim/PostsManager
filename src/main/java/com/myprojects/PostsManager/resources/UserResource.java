45b58eea48cc07e94d94043ef0113cab0c29ac23
Vieira Joaquim <vjoaquim1@gmail.com>
2023-01-31 10:58:46 -0300

    Apagar um user usando o método DELETE

--- src/main/java/com/myprojects/PostsManager/resources/UserResource.java
+++ src/main/java/com/myprojects/PostsManager/resources/UserResource.java
@@ -6,7 +6,9 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -64,5 +66,11 @@ public class UserResource {
 		
 		return ResponseEntity.created(uri).build();
 		 
+	}
+	
+	@DeleteMapping(value="/{id}")
+	public ResponseEntity<Void> delete(@PathVariable String id) {
+		userService.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
